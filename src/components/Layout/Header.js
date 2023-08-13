fc893c95f28466cf8e0e968f306d506b8a714c2f
Abhishek <abhishekbaiju@protonmail.com>
2023-08-13 12:07:28 +0530

    Add Restaurant Summary

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -12,11 +12,13 @@ const Header = () => {
         </div>
         <div class="column header_button has-text-centered">
           <button class="button is-large cart_button">
+            <span class="icon">
+              <i class="fas fa-home"></i>
+            </span>
             Your Cart<p class="cart_item__count">0</p>
           </button>
         </div>
       </div>
-      <div class="background-div"></div>
     </>
   );
 };
---
+++ src/components/Layout/Summary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import bulma from "bulma";
+
+export default function () {
+  return (
+    <div class="background-div">
+      <div class="summary">
+        <h1 class="title is-1 has-text-white">
+          Delicious Food, Delivered To You
+        </h1>
+        <p style={{ textAlign: "center", fontSize: "20px" }}>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home. <br />
+          All our meals are cooked with high-quality ingrediants, just-in-time
+          and of course by experienced chefs!.
+        </p>
+      </div>
+    </div>
+  );
+}
