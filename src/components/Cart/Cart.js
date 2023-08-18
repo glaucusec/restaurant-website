2c4a15fb24dc01fd6dc3eff6f51becf28c9320bb
Abhishek <abhishekbaiju@protonmail.com>
2023-08-18 16:35:11 +0530

    DIY - Showing and Enabling the delete of Cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,15 +6,28 @@ import "./Cart.css";
 
 export default function Cart({ showCart, setShowCart }) {
   const cartCtx = useContext(CartContext);
+
   const [totalAmount, setTotalAmount] = useState(0);
+
   useEffect(() => {
     let total = 0;
     cartCtx.items.forEach((item) => {
       console.log(item);
       total += item.amount * item.meal_price;
     });
-    setTotalAmount(total);
+    setTotalAmount(total.toFixed(2));
   }, [cartCtx.items]);
+
+  function removeItemHandler(id) {
+    cartCtx.removeItem(id);
+  }
+
+  function addItemHandler(item) {
+    console.log(item);
+    const quantity = 1;
+    cartCtx.addItem({ meal: item, amount: quantity });
+  }
+
   return createPortal(
     <>
       <div className={`modal ${showCart}`}>
@@ -22,9 +35,42 @@ export default function Cart({ showCart, setShowCart }) {
         <div class="modal-content">
           <div class="column cart-items">
             {cartCtx.items.map((item) => {
-              return <h2 class="title is-5">{item.meal_name} | Quantity: {item.amount}</h2>;
+              return (
+                <>
+                  <div className="columns">
+                    <div class="column">
+                      <>
+                        <h2 className="title is-3">{item.meal_name}</h2>
+                        <div className="columns">
+                          <div className="column cart_price">
+                            ${item.meal_price}
+                          </div>
+                          <div className="column">
+                            <p className="cart_item_amount">x{item.amount}</p>
+                          </div>
+                        </div>
+                      </>
+                    </div>
+                    <div class="column">
+                      <button
+                        onClick={() => removeItemHandler(item.meal_id)}
+                        className="button"
+                      >
+                        -
+                      </button>
+                      <button
+                        onClick={() => addItemHandler(item)}
+                        className="button"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <hr></hr>
+                </>
+              );
             })}
-            <h2 class="title is-3">Total Amount: ${totalAmount}</h2>
+            <h2 class="title is-4">Total Amount: ${totalAmount}</h2>
             <div class="field is-grouped">
               <div class="control">
                 <button onClick={() => setShowCart("")} class="button is-link">
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,6 +5,7 @@ export default function CartProvider(props) {
   const [cartItems, updateCartItems] = useState([]);
 
   const addItemToCartHandler = ({ meal, amount }) => {
+    console.log({meal, amount})
     const isAlreadyAdded = cartItems.some(
       (item) => item.meal_id === meal.meal_id
     );
@@ -27,7 +28,27 @@ export default function CartProvider(props) {
         ]);
   };
 
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    const mealIndex = cartItems.findIndex((item) => {
+      return item.meal_id == id;
+    });
+    if(cartItems[mealIndex].amount == 1) {
+        const newItems = cartItems.filter(item => {
+            return item.meal_id != id;
+        })
+        updateCartItems(newItems);
+    } else {
+        const newItems = cartItems.map(item => {
+            if(item.meal_id === id) {
+                item.amount -= 1
+                return item;
+            } else {
+                return item;
+            }
+        })
+        updateCartItems(newItems);
+    }
+  };
 
   const cartContext = {
     items: cartItems,
